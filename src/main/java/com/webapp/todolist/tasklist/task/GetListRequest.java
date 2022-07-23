6abe692d658a996911fb40af61882eda3ce0fa2b
RishithGandham <rishith.gandham@gmail.com>
2022-07-23 12:43:49 -0400

    some changes

---
+++ src/main/java/com/webapp/todolist/tasklist/task/GetListRequest.java
@@ -0,0 +1,11 @@
+package com.webapp.todolist.tasklist.task;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class GetListRequest {
+    private Long id;
+}
